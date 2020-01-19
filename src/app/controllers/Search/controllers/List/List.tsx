bf749445900b9c5d3d7d4faa6cfc59ca14b175ee
DavidMBP <steadypathapp@gmail.com>
2020-01-19 17:26:35 -0600

    Connected List

--- src/app/controllers/Search/controllers/List/List.tsx
+++ src/app/controllers/Search/controllers/List/List.tsx
@@ -1,53 +1,32 @@
 import * as React from 'react';
 import UI from './List.ui';
+import { get, map } from 'lodash';
+import { createSelector } from 'reselect';
+import { connect } from 'react-redux';
 import { Repository } from 'types';
 
 const List: React.FC<{
-
+  repos: Repository[]
 }> = ({
-
+  repos
 }) => {
 
   return (
     <UI
-      repos={[test, {...test, id: 12}, {...test, id: 123}]}
+      repos={repos}
     />
   );
 };
-export default React.memo(List);
 
+const getRepos = createSelector(
+  state => get(state, 'models.Repository', {}),
+  reposMap => map(reposMap, repo => repo)
+);
 
-export const test: Repository = {
-  id: 3081286,
-  node_id: 'MDEwOlJlcG9zaXRvcnkzMDgxMjg2',
-  name: 'Tetris',
-  full_name: 'dtrupenn/Tetris',
-  owner: {
-    login: 'dtrupenn',
-    id: 872147,
-    node_id: 'MDQ6VXNlcjg3MjE0Nw==',
-    avatar_url: 'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
-    gravatar_id: '',
-    url: 'https://api.github.com/users/dtrupenn',
-    received_events_url: 'https://api.github.com/users/dtrupenn/received_events',
-    type: 'User'
-  },
-  private: false,
-  html_url: 'https://github.com/dtrupenn/Tetris',
-  description: 'A C implementation of Tetris using Pennsim through LC4 Really Really Long descritions',
-  fork: false,
-  url: 'https://api.github.com/repos/dtrupenn/Tetris',
-  created_at: '2012-01-01T00:31:50Z',
-  updated_at: '2013-01-05T17:58:47Z',
-  pushed_at: '2012-01-01T00:37:02Z',
-  homepage: '',
-  size: 524,
-  stargazers_count: 1,
-  watchers_count: 1,
-  language: 'Assembly',
-  forks_count: 0,
-  open_issues_count: 0,
-  master_branch: 'master',
-  default_branch: 'master',
-  score: 10.309712
-};
+const mapStateToProps = (state) => ({
+  repos: getRepos(state)
+});
+
+export default connect(
+  mapStateToProps
+)(React.memo(List));
