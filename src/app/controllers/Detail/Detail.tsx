761a23c784087f12ee664b9ea1242e36d162dbef
DavidMBP <steadypathapp@gmail.com>
2020-01-20 01:22:20 -0600

    Connected Detail

--- src/app/controllers/Detail/Detail.tsx
+++ src/app/controllers/Detail/Detail.tsx
@@ -1,18 +1,37 @@
 import * as React from 'react';
 import UI from './Detail.ui';
 import { Repository } from 'types';
+import { get } from 'lodash';
 import { RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 const Detail: React.FC<{
-
+  repo: Repository
 } & RouteComponentProps<{id: string}>> = ({
-
+  repo
 }) => {
 
   return (
     <UI
-      repo={{} as Repository}
+      repo={repo}
     />
   );
 };
-export default React.memo(Detail);
+
+
+const makeGetRepo = () => createSelector(
+  (state, props) => get(state, `models.Repository.${props.match.params.id}`, {}),
+  (repo) => repo
+);  // TODO: NEED TO TEST THIS SELECTOR
+
+const mapStateToProps = (state, props) => {
+  const getRepo = makeGetRepo();
+  return {
+    repo: getRepo(state, props)
+  };
+};
+
+export default connect(
+  mapStateToProps
+)(React.memo(Detail));
